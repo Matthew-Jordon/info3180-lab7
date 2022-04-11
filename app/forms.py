20b32075d1d4100e894b187881a4eea08805405a
Matthew-Jordon Whynter <matthewjordon.whynter@mymona.uwi.edu>
2022-04-11 14:16:53 -0500

    made flask form

--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,8 @@
-# Add any form classes for Flask-WTF here
+from wtforms import *
+from flask_wtf import *
+from flask_wtf.file import *
+
+class UploadForm(FlaskForm):
+    description = TextAreaField('description',[validators.InputRequired])
+    photo = FileField('photo', [validators.DataRequired(), FileAllowed(['jpg', 'jpeg', 'png'], 'Images Only!')])
+    csrf_token = csrf.CSRFProtect()
